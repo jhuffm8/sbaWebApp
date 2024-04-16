a8c558a22c11c165400866ba59dd4e1e1b035c38
jhuffm8 <jordan.huffman85@gmail>
2024-04-15 22:03:05 -0600

    added icon

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sbaWebApp
+A weather app that allows search of major cities
+prints out live weather
+such as temp
+forecast
+
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const apiKey ='85db9f63a96a4c34a8b202116241504';
 const form = document.getElementById('weather-form');
 const cityInput = document.getElementById('city');
 const tempDiv = document.getElementById('temp-div');
+const weatherInfo = document.getElementById('weather-info');
+const weatherIcon = document.getElementById('weather-icon');
 
 form.addEventListener('submit', getWeather)
 
@@ -9,10 +11,14 @@ async function getWeather(event) {
     event.preventDefault()
     const city = cityInput.value;
     if(city === ""){
-        alert("Need to enter location")
+        alert("Need to enter location");
+        return
     } else{
     const response = await fetch(` http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
     const data = await response.json()
+    const iconUrl = data.current.condition.icon;
+    const description = data.current.condition.text;
+
     // weatherData.innerHTML = `<h2>${data.location.name}</h2>
     // <p>Local Time: ${data.location.localtime}</p>
     // <p>Tempature in Fahrenheit: ${data.current.temp_f}</p>
@@ -20,8 +26,20 @@ async function getWeather(event) {
     // <p>Condition: ${data.current.condition.text}</p>
     // `
     tempDiv.innerHTML = `<p>${data.current.temp_f}</p>`
+    weatherInfo.innerHTML = `<p>${data.current.condition.text}</p>`
+    weatherIcon.src = `https:${iconUrl}`;
+    showImage();
+
+
+
+
 
     console.log(data)
+    // console.log(iconUrl)
     }
 }
 // getWeather();
+
+function showImage(){
+    weatherIcon.style.display = 'block'
+}
